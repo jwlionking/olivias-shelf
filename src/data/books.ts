3f1cad6de835bd051755ce1f9ae5a65dad5c0d9d
CrimsonFusion <jwlionking@yahoo.com>
2026-09-12 21:47:36 -0400

    Add five Elon and Olivia physics books to the shelf

--- src/data/books.ts
+++ src/data/books.ts
@@ -1,9 +1,10 @@
 import { freeBooks } from "./books-free";
+import { elonSeriesBooks } from "./elon-series";
 import { learnBooks } from "./books-learn";
 import { memberBooks } from "./books-member";
 import type { Book } from "./types";
 
-export const BOOKS: Book[] = [...freeBooks, ...memberBooks, ...learnBooks];
+export const BOOKS: Book[] = [...freeBooks, ...elonSeriesBooks, ...memberBooks, ...learnBooks];
 
 export function getBook(slug: string): Book | undefined {
   return BOOKS.find((b) => b.slug === slug);
